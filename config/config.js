c31a475d9ce47a52b31859c5dde422f4ae51d3ee
Álvaro <133866382+Alvaro060@users.noreply.github.com>
2025-03-03 17:05:22 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,13 +3,13 @@ require("dotenv").config({
 });
 
 module.exports = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 8083,
   db: {
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "test",
     name: process.env.DB_NAME || "reservasHotelReact",
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 54577,
   },
   secretKey: process.env.SECRET_KEY || "default_secret",
 };
